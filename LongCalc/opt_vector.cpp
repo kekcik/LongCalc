f8837c36502fe55f50a697ede7a3edeb9d6efc21
Ivan Trofimov <richard1997dog@gmail.com>
2016-10-28 09:59:25 +0300

    add files

---
+++ LongCalc/opt_vector.cpp
@@ -0,0 +1,9 @@
+//
+//  opt_vector.cpp
+//  LongCalc
+//
+//  Created by Иван Трофимов on 28.10.16.
+//  Copyright © 2016 Иван Трофимов. All rights reserved.
+//
+
+#include "opt_vector.hpp"
---
+++ LongCalc/opt_vector.hpp
@@ -0,0 +1,14 @@
+//
+//  opt_vector.hpp
+//  LongCalc
+//
+//  Created by Иван Трофимов on 28.10.16.
+//  Copyright © 2016 Иван Трофимов. All rights reserved.
+//
+
+#ifndef opt_vector_hpp
+#define opt_vector_hpp
+
+#include <stdio.h>
+
+#endif /* opt_vector_hpp */
